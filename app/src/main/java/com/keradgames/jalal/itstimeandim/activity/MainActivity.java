6ca921233e40bfc8318c42979c235f01ebd120f8
Otto Zerpa <otto.zerpa@gmail.com>
2015-05-06 23:10:46 +0200

    Improved UI

--- app/src/main/java/com/keradgames/jalal/itstimeandim/activity/MainActivity.java
+++ app/src/main/java/com/keradgames/jalal/itstimeandim/activity/MainActivity.java
@@ -3,14 +3,19 @@ package com.keradgames.jalal.itstimeandim.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.keradgames.jalal.itstimeandim.R;
+import com.keradgames.jalal.itstimeandim.util.RoundedTransformation;
 import com.keradgames.jalal.itstimeandim.viewmodel.OnViewModelDataReady;
 import com.keradgames.jalal.itstimeandim.viewmodel.TweetViewModel;
+import com.squareup.picasso.Picasso;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import rx.subscriptions.CompositeSubscription;
 import twitter4j.Status;
@@ -21,8 +26,6 @@ public class MainActivity extends Activity implements OnViewModelDataReady {
 
     private CompositeSubscription mCompositeSubscription = new CompositeSubscription();
     private TweetViewModel mViewModel;
-
-    private ViewGroup mContainer;
 
     private Status mTweet;
 
@@ -35,7 +38,6 @@ public class MainActivity extends Activity implements OnViewModelDataReady {
             mTweet = (Status)savedInstanceState.getSerializable(SAVED_TWEET);
         }
 
-        mContainer = (LinearLayout)findViewById(R.id.container);
         mViewModel = new TweetViewModel(this, mCompositeSubscription);
     }
 
@@ -54,9 +56,35 @@ public class MainActivity extends Activity implements OnViewModelDataReady {
     @Override
     public void onComplete(Status tweet) {
         mTweet = tweet;
-        TextView text = new TextView(this);
-        text.setText(tweet.getText());
-        mContainer.addView(text);
+
+        ImageView profilePic = (ImageView)findViewById(R.id.profile_pic);
+        loadImage(profilePic);
+
+        ((TextView)findViewById(R.id.profile_name)).setText(mTweet.getUser().getName());
+        ((TextView)findViewById(R.id.twitter_name)).setText("@" + mTweet.getUser().getScreenName());
+        ((TextView)findViewById(R.id.tweet_text)).setText(mTweet.getText());
+        ((TextView)findViewById(R.id.tweet_time)).setText(getTweetTime(mTweet.getCreatedAt().getTime()));
+        ((TextView)findViewById(R.id.retweet_count)).setText(getRetweetText(mTweet.getRetweetCount()));
+    }
+
+    private void loadImage(ImageView view) {
+        int size = getResources().getDimensionPixelSize(R.dimen.imageSize);
+        Picasso.with(this).load(mTweet.getUser().getOriginalProfileImageURL())
+                .transform(new RoundedTransformation(10,0))
+                .resize(size, size)
+                .placeholder(R.drawable.image_placeholder)
+                .into(view);
+    }
+
+    private String getTweetTime(long tweetMillis) {
+        String format = "hh:mm a - dd MMM yyyy";
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(format);
+        DateTime date = new DateTime(tweetMillis);
+        return formatter.print(date);
+    }
+
+    private String getRetweetText(int numRetweets) {
+        return  getResources().getQuantityString(R.plurals.retweet_sufix, numRetweets, numRetweets);
     }
 
     @Override
